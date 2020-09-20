d4cfe230f620314602497f27b7bf193ae42d2e8e
pubududp <pubz87@gmail.com>
2020-09-20 01:52:22 +0100

    JSON response

--- index.js
+++ index.js
@@ -4,8 +4,45 @@ const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     if(req.url==='/'){
-        res.end('<h1>Homepage</h1>');
+        fs.readFile(
+            path.join(__dirname,'public','index.html'),
+            (err, content) => {   
+                if(err) throw err;     
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.writeHead(200,{'test':'test'});
+            res.end(content);
+        }
+    );
+        
     }
+
+    if(req.url==='/about'){
+        fs.readFile(
+            path.join(__dirname,'public','about.html'),
+            (err, content) => {   
+                if(err) throw err;     
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.writeHead(200,{'test':'test'});
+            res.end(content);
+        }
+    );
+    }
+
+    if(req.url==='/api/users'){
+        const users = [
+            {name:'John',age: 40},
+            {name:'Mark',age:33}
+        ]
+
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(users));
+
+        
+    }
+        
+    
+
+
 });
 
 const PORT = process.env.PORT || 5000;
