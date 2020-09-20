c5234e57435632217eef033446662bfc61315e30
pubududp <pubz87@gmail.com>
2020-09-20 02:29:20 +0100

    update response

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
+
 const server = http.createServer((req,res)=>{
+    
+  /*  
     if(req.url==='/'){
         fs.readFile(
             path.join(__dirname,'public','index.html'),
@@ -35,13 +38,61 @@ const server = http.createServer((req,res)=>{
         ]
 
         res.writeHead(200,{'Content-Type':'application/json'});
-        res.end(JSON.stringify(users));
+        res.end(JSON.stringify(users));       
+    }
+    */
+
+    //Dynamic file path
+    let filePath = path.join(
+        __dirname,
+        'public',
+        req.url==='/' ? 'index.html' : req.url
+    );
+
+    //Extension of file
+    let extname = path.extname(filePath);
+
+    //Set content type based on ext
+    switch(extname){
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
 
         
     }
-        
-    
 
+    //Read file
+    fs.readFile(filePath, (err, content) => {
+        if(err){
+            if(err.code=== 'ENOENT'){
+                //404
+                fs.readFile(path.join(__dirname,'public','404.html'), (err,content) => {
+                    res.writeHead(200,{'Content-Type':'text/html'});
+                    res.end(content, 'utf8');
+                })
+            } else {
+                //500
+                res.writeHead(500);
+                res.end(`Server error: ${err.code}`);
+            }
+        } else {
+            //200
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content,'utf8');
+        }
+    });
 
 });
 
